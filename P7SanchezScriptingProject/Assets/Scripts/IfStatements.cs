b840d19fd5ed0d72100e6c6969fd4d0349bb01cf
752071-tech <752071@student.dusd.net>
2025-11-05 14:57:27 -0800

    fixed the code
    I fixed the if statements code

--- P7SanchezScriptingProject/Assets/Scripts/IfStatements.cs
+++ P7SanchezScriptingProject/Assets/Scripts/IfStatements.cs
@@ -21,4 +21,27 @@ public class IfStatements : MonoBehaviour
 
         coffeeTemperature -= Time.deltaTime * 5f;
     }
+
+    void TemperatureTest()
+    {
+        // If the coffee's temperature is greater than the hottest drinking temperature...
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            // ... do this.
+            print("Coffee is too hot.");
+        }
+        // If it isn't, but the coffee temperature is less than the coldest drinking temperature...
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
+            // ... do this.
+            print("Coffee is too cold.");
+        }
+        // If it is neither of those then...
+        else
+        {
+            // ... do this.
+            print("Coffee is just right.");
+        }
+    }
 }
+    
