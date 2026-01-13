49a7901302b087ef443d97fc56971a649337aee1
752071-tech <752071@student.dusd.net>
2026-01-13 14:16:30 -0800

    i added update and fixupdate
    i finished the scripting assignment.

---
+++ P7SanchezScriptingProject/Assets/Scripts/Update and FixedUpdate.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UpdateAndFixedUpdate : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate time :" + Time.deltaTime);
+    }
+
+
+    void Update()
+    {
+        Debug.Log("Update time :" + Time.deltaTime);
+    }
+}
