aa0aa36a851e4dfc5a59ad1a7475da6ea2fc88dd
752071-tech <752071@student.dusd.net>
2026-02-05 14:23:03 -0800

    finished scripting assignment
    finished linear interpolation

---
+++ P7SanchezScriptingProject/Assets/Scripts/Linear Interpolation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{ /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+//Example this line of code makes it so that...
+        
+    }
+
+*/
+
+}
